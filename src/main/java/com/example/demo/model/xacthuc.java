9c81e8779f259c7d2d351e108e1d0150d6dfe66b
Myanh-ng <nguyenthimyanh005@gmail.com>
2025-10-11 15:14:08 +0700

    Cập nhật chức năng đăng nhập

---
+++ src/main/java/com/example/demo/model/xacthuc.java
@@ -0,0 +1,64 @@
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+
+
+@Entity
+@Table(name = "xacthuc")
+public class xacthuc {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sdt;
+    private String cccd;
+    private String ten;
+    private String gioitinh;
+    private String bienso;
+
+    public xacthuc() {}
+    public xacthuc(Long id, String sdt, String cccd, String ten, String gioitinh, String bienso) {
+        this.id = id;
+        this.sdt = sdt;
+        this.cccd = cccd;
+        this.ten = ten;
+        this.gioitinh = gioitinh;
+        this.bienso = bienso;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getSdt() {
+        return sdt;
+    }
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+    public String getCccd() {
+        return cccd;
+    }
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
+    }
+    public String getTen() {
+        return ten;
+    }
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+    public String getGioitinh() {
+        return gioitinh;
+    }
+    public void setGioitinh(String gioitinh) {
+        this.gioitinh = gioitinh;
+    }
+}
